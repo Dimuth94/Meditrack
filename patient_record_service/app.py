c80d736981695ed44b48af7adb7decda1b407d52
Lenovo@CSI-PF0YHC36 MINGW64 /c/Dimuth/MSC_docs/1st_semester/CloudComputing/CW/meditrack$ git config --global user.name Dimuth <dimuthr1994@gmail.com>
2024-12-19 10:18:37 +0530

    add comment

--- patient_record_service/app.py
+++ patient_record_service/app.py
@@ -20,6 +20,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
+#adding comment
 
 # Ensure DB_PORT is valid and is an integer
 try:
